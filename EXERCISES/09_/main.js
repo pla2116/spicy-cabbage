bb3c21b1ec1fdc4ffa4e94bf7bd6baa52608ee78
pla2116 <pla2116@columbia.edu>
2023-04-04 19:46:33 -0400

    poem generator

---
+++ EXERCISES/09_/main.js
@@ -0,0 +1,34 @@
+console.log("This code is loading the JavaScript file");
+
+let materials = ["wood", "brick", "broken dishes", "dust", "leaves", "grass"];
+
+let places = [
+  "cold, windy climate",
+  "desert",
+  "deserted airport",
+  "deserted church",
+  "deserted factory",
+  "hot climate",
+  "metropolis"
+];
+
+let people = [
+  "collectors of all types",
+  "fishermen and families",
+  "french and german speaking people",
+  "old friends",
+  "horses and birds",
+  "little boys",
+  "lovers"
+];
+
+let thing = [
+  "natural light",
+  "all available lighting",
+  "candles",
+  "electricity"
+];
+
+// select a random element from each array
+// write a function generatePoem() render this randomized text to the page
+// have this function run on window load
