9df8fe10b93b29a589f5c5cd277802b529c7e9e7
pla2116 <pla2116@columbia.edu>
2023-04-11 20:40:33 -0400

    Update main.js

--- EXERCISES/11_Filter_Array_Flowers/main.js
+++ EXERCISES/11_Filter_Array_Flowers/main.js
@@ -37,7 +37,32 @@ const flowers = [
 
 const ul = document.querySelector("ul");
 
-function renderFlowersToPage(results) {}
+function renderFlowersToPage(results) {
+  // iterate over data set
+  for(let i = 0; i < results.length; i++){
+    // create the list item
+    let listItem = document.createElement('li');
+    // add a class to each item of the results
+    listItem.classList.add('card', results[i].color) // red
+    // add flower name
+    let title = document.createElement('h3')
+    title.textContent = results[i].name // Rose
+    // add flower color
+    let color = document.createElement('p')
+    color.classList.add(results[i].color)
+    color.textContent = results[i].color
+
+    // add flower image
+    let image = document.createElement('img')
+    image.setAttribute('src', results[i].image)
+
+    ul.appendChild(listItem)
+    listItem.appendChild(title)
+    listItem.appendChild(color)
+    listItem.appendChild(image)
+
+  }
+}
 renderFlowersToPage(flowers);
 
 //---------- FILTER FLOWERS BY COLOR
@@ -45,5 +70,28 @@ renderFlowersToPage(flowers);
 let filterBtns = document.querySelector(".filters");
 let cards = document.querySelectorAll(".card");
 
-function filterFn(e) {}
+function filterFn(event) {
+  console.log(event.target)
+  if(event.target.classList.contains('filter-btn')){
+    // select the current active button
+    let activeBtn = filterBtns.querySelector('.active')
+    activeBtn.classList.remove('active')
+
+    // apply the active class to the target
+    event.target.classList.add('active')
+
+    const filterValue = event.target.getAttribute('data-filter') // yellow | red
+
+    for(let i = 0; i < cards.length; i++){
+      if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
+        cards[i].classList.remove('hide')
+        cards[i].classList.add('show')
+      } else {
+        cards[i].classList.remove('show')
+        cards[i].classList.add('hide')
+      }
+
+    }
+  }
+}
 filterBtns.addEventListener("click", filterFn);
