61cbff80e23cdb0471a781ca5c408a849c7cc858
pla2116 <pla2116@columbia.edu>
2023-05-02 20:57:50 -0400

    Update main.js

--- PROJECTS/02/main.js
+++ PROJECTS/02/main.js
@@ -6,3 +6,17 @@ btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
 	number += Math.ceil(Math.random() * 1000);
 }
+
+const perfumes = [
+    {
+      name: "Mandorlo di Sicilia",
+      color: "blue",
+      image:
+        "https://cdn.britannica.com/37/227037-050-CA792866/Broken-tulip-flower.jpg"
+    },
+    {
+      name: "Acqua Di Gio",
+      color: "orange",
+      image: "https://h2.commercev3.net/cdn.brecks.com/images/800/67248A.jpg"
+    }
+  ];
