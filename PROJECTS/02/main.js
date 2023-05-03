595bd9f09376e0659c5fbc0ac5d2af1303808ec8
pla2116 <pla2116@columbia.edu>
2023-05-03 01:52:44 -0400

    updates

--- PROJECTS/02/main.js
+++ PROJECTS/02/main.js
@@ -1,22 +1,74 @@
-let container = document.querySelector(".container");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 1000);
+console.log('this works')
 
-btn.onclick = function () {
-	container.style.transform = "rotate(" + number + "deg)";
-	number += Math.ceil(Math.random() * 1000);
+//---------- DATASET
+
+const flowers = [
+  {
+    name: "Mandorlo di Sicilia",
+    color: "blue",
+    image: "../../ASSETS/Perfumes/Acqua Di Parma_Mandorlo di Sicilia.jpg"
+  }
+];
+
+//---------- RENDER FLOWERS TO PAGE
+
+const ul = document.querySelector("ul");
+
+function renderFlowersToPage(results) {
+  // iterate over data set
+  for(let i = 0; i < results.length; i++){
+    // create the list item
+    let listItem = document.createElement('li');
+    // add a class to each item of the results
+    listItem.classList.add('card', results[i].color) // red
+    // add flower name
+    let title = document.createElement('h3')
+    title.textContent = results[i].name // Rose
+    // add flower color
+    let color = document.createElement('p')
+    color.classList.add(results[i].color)
+    color.textContent = results[i].color
+
+    // add flower image
+    let image = document.createElement('img')
+    image.setAttribute('src', results[i].image)
+
+    ul.appendChild(listItem)
+    listItem.appendChild(title)
+    listItem.appendChild(color)
+    listItem.appendChild(image)
+
+  }
 }
+renderFlowersToPage(flowers);
 
-const perfumes = [
-    {
-      name: "Mandorlo di Sicilia",
-      color: "blue",
-      image:
-        "https://cdn.britannica.com/37/227037-050-CA792866/Broken-tulip-flower.jpg"
-    },
-    {
-      name: "Acqua Di Gio",
-      color: "orange",
-      image: "https://h2.commercev3.net/cdn.brecks.com/images/800/67248A.jpg"
+//---------- FILTER FLOWERS BY COLOR
+
+let filterBtns = document.querySelector(".filters");
+let cards = document.querySelectorAll(".card");
+
+function filterFn(event) {
+  console.log(event.target)
+  if(event.target.classList.contains('filter-btn')){
+    // select the current active button
+    let activeBtn = filterBtns.querySelector('.active')
+    activeBtn.classList.remove('active')
+
+    // apply the active class to the target
+    event.target.classList.add('active')
+
+    const filterValue = event.target.getAttribute('data-filter') // yellow | red
+
+    for(let i = 0; i < cards.length; i++){
+      if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
+        cards[i].classList.remove('hide')
+        cards[i].classList.add('show')
+      } else {
+        cards[i].classList.remove('show')
+        cards[i].classList.add('hide')
+      }
+
     }
-  ];
+  }
+}
+filterBtns.addEventListener("click", filterFn);
